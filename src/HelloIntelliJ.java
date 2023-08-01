1cc0c7ec853131421baa74d9aef7ab796e3f5b04
ggagne <ggagne@westminsteru.edu>
2023-08-01 16:01:45 -0600

    Added HelloIntelliJ class

---
+++ src/HelloIntelliJ.java
@@ -0,0 +1,9 @@
+/**
+ * This is your first Java program using IntelliJ
+ */
+public class HelloIntelliJ {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java programming using IntelliJ!");
+    }
+
+}
